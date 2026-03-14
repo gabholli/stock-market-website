b5b010516fec13bcbb19ebff35fad88bf376e826
Gabe Hollifield <gholl467@gmail.com>
2026-03-13 23:42:37 -0700

    Added api routes router

--- server.ts
+++ server.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import cors from "cors"
+import { apiRouter } from "./src/backend/routes/apiRoutes.ts"
 
 const PORT = 8000
 
 const app = express()
 
 app.use(cors())
+
+app.use("/users", apiRouter)
 
 app.use((req, res) => {
     res.send("Server here...")
