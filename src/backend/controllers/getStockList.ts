a2db22b73306f603c92cb3156501b24657151484
Gabe Hollifield <gholl467@gmail.com>
2026-03-16 21:12:14 -0700

    Update getStockList.ts

--- src/backend/controllers/getStockList.ts
+++ src/backend/controllers/getStockList.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import type { Response } from "express";
 
-export default async function getStockList(res: Response) {
+export default async function getStockList(req: Request, res: Response) {
     try {
         const response = await axios.get(`https://api.twelvedata.com/price?symbol=AAPL&apikey=${process.env.DATA_KEY}`)
         console.log(response.data)
+        console.log(req.body)
 
         res.status(200).json(response.data)
     } catch (err) {
