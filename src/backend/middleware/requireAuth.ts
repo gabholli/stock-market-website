4cb83d0885dbbe38d5e519d40381ac7746ac01f6
Gabe Hollifield <gholl467@gmail.com>
2026-03-19 15:47:04 -0700

    Updated types in require auth middleware

--- src/backend/middleware/requireAuth.ts
+++ src/backend/middleware/requireAuth.ts
@@ -1,5 +1,7 @@
-export default function requireAuth(req: { session: { userId: any } }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { error: string }): any; new(): any } } }, next: () => void) {
-    if (!req.session.userId) {
+import type { Request, Response, NextFunction } from 'express'
+
+export default function requireAuth(req: Request, res: Response, next: NextFunction) {
+    if (!req.session.id) {
         console.log("Access to protected route blocked")
         return res.status(401).json({ error: "Unauthorized" })
     }
