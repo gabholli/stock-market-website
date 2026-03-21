c224e62876b0e68f279e2b3fe55a9515a7884569
Gabe Hollifield <gholl467@gmail.com>
2026-03-21 13:26:01 -0700

    Update getStockList.ts

--- src/backend/controllers/getStockList.ts
+++ src/backend/controllers/getStockList.ts
@@ -7,7 +7,7 @@ export default async function getStockList(req: Request, res: Response) {
     if (cached) return res.status(200).json(cached)
     console.log(req.query.symbol)
 
-    const response = await axios.get(`https://api.twelvedata.com/quote?symbol=AAPL&apikey=${process.env.DATA_KEY}`)
+    const response = await axios.get(`https://api.twelvedata.com/quote?symbol=MUB&apikey=${process.env.DATA_KEY}`)
     const data = await response.data
 
     cache.set('stockData', data)
