1dc767db30d035e9f0d10e6ec38b811b2be6a80f
Gabe Hollifield <gholl467@gmail.com>
2026-03-21 15:17:52 -0700

    Made it so card only renders after a search

--- src/pages/data..tsx
+++ src/pages/data..tsx
@@ -77,35 +77,37 @@ export default function Data() {
                 </div>
             </form >
             <main className="flex flex-col flex-1 items-center text-center gap-y-8 mt-8">
+                {stockInfo && timeSeries && (
 
-                <section className="bg-white flex flex-col gap-y-2 text-left p-4 md:p-12 shadow-xl">
-                    <h1 className="text-lg">{stockInfo?.symbol}</h1>
-                    <p className="underline text-lg">{stockInfo?.name}</p>
-                    <div className="flex flex-col gap-y-2">
-                        {parseInt(stockInfo?.change ?? "0") > 0 ? (
-                            <p>Stock Change: ${parseFloat(stockInfo?.change?.substring(1) ?? "N/A").toFixed(2)}</p>
-                        ) :
-                            <p>Stock Change: -${parseFloat(stockInfo?.change?.substring(1) ?? "N/A").toFixed(2)}</p>
-                        }
-                        {parseInt(stockInfo?.close ?? "0") > 0 ? (
-                            <p>Last Closing Price: ${parseFloat(stockInfo?.close?.substring(1) ?? "N/A").toFixed(2)}</p>
-                        ) :
-                            <p>Last Closing Price: -${parseFloat(stockInfo?.close?.substring(1) ?? "N/A").toFixed(2)}</p>
-                        }
-                        <p>Exchange: {stockInfo?.exchange}</p>
-                        {parseInt(stockInfo?.high ?? "0") > 0 ? (
-                            <p>Highest Price: ${parseFloat(stockInfo?.high?.substring(1) ?? "N/A").toFixed(2)}</p>
-                        ) :
-                            <p>Highest Price: -${parseFloat(stockInfo?.high?.substring(1) ?? "N/A").toFixed(2)}</p>
-                        }
-                        {parseInt(stockInfo?.low ?? "0") > 0 ? (
-                            <p>Lowest Price: ${parseFloat(stockInfo?.low?.substring(1) ?? "N/A").toFixed(2)}</p>
-                        ) :
-                            <p>Lowest Price: -${parseFloat(stockInfo?.low?.substring(1) ?? "N/A").toFixed(2)}</p>
-                        }
-                    </div>
-                    <button className="mt-2 px-4 py-2 bg-blue-500 text-white cursor-pointer">Add to watch list</button>
-                </section>
+                    <section className="bg-white flex flex-col gap-y-2 text-left p-4 md:p-12 shadow-xl">
+                        <h1 className="text-lg">{stockInfo?.symbol}</h1>
+                        <p className="underline text-lg">{stockInfo?.name}</p>
+                        <div className="flex flex-col gap-y-2">
+                            {parseInt(stockInfo?.change ?? "0") > 0 ? (
+                                <p>Stock Change: ${parseFloat(stockInfo?.change?.substring(1) ?? "N/A").toFixed(2)}</p>
+                            ) :
+                                <p>Stock Change: -${parseFloat(stockInfo?.change?.substring(1) ?? "N/A").toFixed(2)}</p>
+                            }
+                            {parseInt(stockInfo?.close ?? "0") > 0 ? (
+                                <p>Last Closing Price: ${parseFloat(stockInfo?.close?.substring(1) ?? "N/A").toFixed(2)}</p>
+                            ) :
+                                <p>Last Closing Price: -${parseFloat(stockInfo?.close?.substring(1) ?? "N/A").toFixed(2)}</p>
+                            }
+                            <p>Exchange: {stockInfo?.exchange}</p>
+                            {parseInt(stockInfo?.high ?? "0") > 0 ? (
+                                <p>Highest Price: ${parseFloat(stockInfo?.high?.substring(1) ?? "N/A").toFixed(2)}</p>
+                            ) :
+                                <p>Highest Price: -${parseFloat(stockInfo?.high?.substring(1) ?? "N/A").toFixed(2)}</p>
+                            }
+                            {parseInt(stockInfo?.low ?? "0") > 0 ? (
+                                <p>Lowest Price: ${parseFloat(stockInfo?.low?.substring(1) ?? "N/A").toFixed(2)}</p>
+                            ) :
+                                <p>Lowest Price: -${parseFloat(stockInfo?.low?.substring(1) ?? "N/A").toFixed(2)}</p>
+                            }
+                        </div>
+                        <button className="mt-2 px-4 py-2 bg-blue-500 text-white cursor-pointer">Add to watch list</button>
+                    </section>
+                )}
             </main>
         </>
     )
