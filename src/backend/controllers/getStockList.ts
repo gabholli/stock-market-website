8e15ee961474985ba8344c43366b8dc2dbb0a801
Gabe Hollifield <gholl467@gmail.com>
2026-03-21 12:28:21 -0700

    Added axios to stock api controller

--- src/backend/controllers/getStockList.ts
+++ src/backend/controllers/getStockList.ts
@@ -1,13 +1,14 @@
 import type { Request, Response } from 'express'
 import { cache } from '../cache.ts'
+import axios from 'axios'
 
 export default async function getStockList(req: Request, res: Response) {
     const cached = cache.get('stockData')
     if (cached) return res.status(200).json(cached)
     console.log(req.query)
 
-    const response = await fetch(`https://api.twelvedata.com/quote?symbol=AAPL&apikey=${process.env.DATA_KEY}`)
-    const data = await response.json()
+    const response = await axios.get(`https://api.twelvedata.com/quote?symbol=AAPL&apikey=${process.env.DATA_KEY}`)
+    const data = await response.data
 
     cache.set('stockData', data)
     return res.status(200).json(data)
