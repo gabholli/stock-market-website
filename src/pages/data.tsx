28971a146c7c778aaeb1d34e1b5d518953aa5ff3
Gabe Hollifield <gholl467@gmail.com>
2026-03-23 14:22:05 -0700

    Added rounded corners for data card

--- src/pages/data.tsx
+++ src/pages/data.tsx
@@ -75,7 +75,7 @@ export default function Data() {
                 gap-y-8 mt-8 p-4">
                 {stockInfo && timeSeries && timeSeries.values ? (
                     <>
-                        <section className="bg-white flex flex-col gap-y-2 text-left p-4 md:p-12 shadow-xl">
+                        <section className="bg-white flex flex-col gap-y-2 text-left p-4 md:p-12 shadow-xl rounded-xl">
                             <h1 className="text-lg">{stockInfo?.symbol}</h1>
                             <p className="underline text-lg">{stockInfo?.name}</p>
                             <div className="flex flex-col gap-y-2">
