99aa4e9d1f406e266438c92037013dd5e405b15b
Gabe Hollifield <gholl467@gmail.com>
2026-03-22 20:06:07 -0700

    Updated spinner color to blue

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ export default function Loading() {
         <div className="flex flex-col flex-1 justify-center items-center text-center">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path
-                    fill="currentColor"
+                    fill="#0000FF"
                     d="M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z">
                     <animateTransform
                         attributeName="transform"
