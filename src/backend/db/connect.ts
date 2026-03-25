cd97cc63da73efc0f43180b901c9205068754260
Gabe Hollifield <gholl467@gmail.com>
2026-03-25 12:59:58 -0700

    Update connect.ts

--- src/backend/db/connect.ts
+++ src/backend/db/connect.ts
@@ -1,22 +1,10 @@
-import mongoose from "mongoose"
+import { MongoClient } from "mongodb";
 
 export default async function getDBConnection() {
-    const uri = process.env.ATLAS_URI
 
-    if (!uri) {
-        throw new Error("ATLAS_URL environment variable is not set")
-    }
-
-    const safeUri: string = uri
-
-    try {
-        await mongoose.connect(safeUri, {
-            serverSelectionTimeoutMS: 30000,
-            ssl: true,
-        })
-
-        console.log("✅ Connected to MongoDB Atlas via Mongoose")
-    } catch (err) {
-        console.error("❌ MongoDB connection error:", err)
-    }
+    const db = process.env.ATLAS_URI
+    if (!db) throw new Error("ATLAS_URI environment variable is not set")
+    const client = new MongoClient(db)
+    console.log("Connect to database")
+    await client.connect()
 }
