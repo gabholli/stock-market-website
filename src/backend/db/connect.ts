4a94bc02b9d0f301b161e5ac798b7a40b99c5549
Gabe Hollifield <gholl467@gmail.com>
2026-03-25 11:51:02 -0700

    Changed console log in connection file

--- src/backend/db/connect.ts
+++ src/backend/db/connect.ts
@@ -5,6 +5,6 @@ export default async function getDBConnection() {
     const db = process.env.ATLAS_URI
     if (!db) throw new Error("ATLAS_URI environment variable is not set")
     const client = new MongoClient(db)
-    console.log("Connect to database")
+    console.log("Connected to database")
     await client.connect()
 }
