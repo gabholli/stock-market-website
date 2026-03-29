0f3f1a7ba83d60e2e44b3aaed123e2b6ace91baf
Gabe Hollifield <gholl467@gmail.com>
2026-03-29 13:01:18 -0700

    Added middleware back and uncommented some code

--- src/backend/routes/watchListRouter.ts
+++ src/backend/routes/watchListRouter.ts
@@ -1,9 +1,9 @@
 import express from "express"
 import { addToWatchLIst, deleteWatchlistItem, getAll } from "../controllers/watchListController.ts"
-// import requireAuth from "../middleware/requireAuth.ts"
+import requireAuth from "../middleware/requireAuth.ts"
 
 export const watchListRouter = express.Router()
 
-watchListRouter.post("/", addToWatchLIst)
-watchListRouter.get("/all", getAll)
-watchListRouter.delete("/:symbol", deleteWatchlistItem)
+watchListRouter.post("/", requireAuth, addToWatchLIst)
+watchListRouter.get("/all", requireAuth, getAll)
+watchListRouter.delete("/:symbol", requireAuth, deleteWatchlistItem)
