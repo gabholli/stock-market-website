660160e60c68552aa2f80354e6ad0de5e1b5195b
Gabe Hollifield <gholl467@gmail.com>
2026-03-29 11:38:03 -0700

    Installed connect mongo and updated server file

--- server.ts
+++ server.ts
@@ -6,15 +6,22 @@ import dotenv from "dotenv"
 import { watchListRouter } from "./src/backend/routes/watchListRouter.ts"
 import { authRouter } from "./src/backend/routes/authRoutes.ts"
 import { meRouter } from "./src/backend/routes/meRoutes.ts"
-
-const app = express()
+import MongoStore from "connect-mongo"
 
 dotenv.config()
 
+const app = express()
+
 const PORT = process.env.PORT || 3000
 const secret = process.env.SECRET_KEY
+const mongoUri = process.env.ATLAS_URI
+
 if (!secret) {
     throw new Error("SECRET_KEY environment variable is not set")
+}
+
+if (!mongoUri) {
+    throw new Error("ATLAS_URI environment variable is not setz")
 }
 
 app.use(cors())
@@ -27,6 +34,9 @@ app.use(session({
     secret: secret,
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: mongoUri
+    }),
     cookie: {
         httpOnly: true,
         secure: false,
