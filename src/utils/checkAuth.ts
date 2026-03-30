642eb6e9bf8c6503ce3ffd9d05263377156ad494
Gabe Hollifield <gholl467@gmail.com>
2026-03-29 22:00:43 -0700

    Update checkAuth.ts

--- src/utils/checkAuth.ts
+++ src/utils/checkAuth.ts
@@ -3,12 +3,11 @@ import api from "../backend/api"
 export async function checkAuth() {
     try {
         const res = await api.get("/auth/me")
-
         const user = res.data
         if (!user.isLoggedIn) {
             return false
         }
-        return user.email
+        return true
 
     } catch (err) {
         console.log(err, 'Auth check failed')
