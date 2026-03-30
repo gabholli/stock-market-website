65d8d7c69dc9321e4ca32a2148ea3a65c69f16a1
Gabe Hollifield <gholl467@gmail.com>
2026-03-30 13:45:54 -0700

    Added text to home page

--- src/frontend/pages/home.tsx
+++ src/frontend/pages/home.tsx
@@ -3,9 +3,10 @@ export default function Home() {
         <>
 
             <main
-                className="flex flex-1 justify-center items-center"
+                className="flex flex-1 flex-col justify-center items-center gap-y-4"
             >
                 <h1 className="text-xl md:text-3xl">Welcome to Stock Pulse</h1>
+                <p className="text-lg md:text-2xl">Sign in to store stocks to watchlist</p>
             </main>
 
         </>
