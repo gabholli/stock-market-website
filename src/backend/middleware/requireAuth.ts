2000c6b2c9a0e72d778ed1dad14b886b36177d19
Gabe Hollifield <gholl467@gmail.com>
2026-03-29 21:19:23 -0700

    Update requireAuth.ts

--- src/backend/middleware/requireAuth.ts
+++ src/backend/middleware/requireAuth.ts
@@ -1,6 +1,8 @@
 import type { Request, Response, NextFunction } from 'express'
 
 export default function requireAuth(req: Request, res: Response, next: NextFunction) {
+    console.log("Session: ", req.session)
+    console.log("userId: ", req.session.userId)
     if (!req.session.userId) {
         console.log("Access to protected route blocked")
         return res.status(401).json({ error: "Unauthorized" })
