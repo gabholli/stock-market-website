26abcd5ec979d6881b960ec5ba3d5f98bf9044d9
Gabe Hollifield <gholl467@gmail.com>
2026-03-29 23:14:18 -0700

    Updated semantic tags in header and fixed syntax error in main.tsx

--- src/frontend/components/Header.tsx
+++ src/frontend/components/Header.tsx
@@ -21,35 +21,33 @@ export default function Header() {
     }
 
     return (
-        <>
-            <main className="bg-blue-500 text-white flex flex-col lg:flex-row pt-4 md:pt-0 md:mt-0 justify-between items-center lg:pl-24">
-                <section className="lg:p-8 md:pt-6">
-                    <h1 className="text-xl md:text-4xl font-extrabold">Stock Pulse</h1>
-                </section>
-                <nav className="flex items-center justify-center flex-wrap gap-4 lg:justify-end lg:pr-24 gap-x-8 p-4 md:p-6 lg:p-8
+        <header className="bg-blue-500 text-white flex flex-col lg:flex-row pt-4 md:pt-0 md:mt-0 justify-between items-center lg:pl-24">
+            <section className="lg:p-8 md:pt-6">
+                <h1 className="text-xl md:text-4xl font-extrabold">Stock Pulse</h1>
+            </section>
+            <nav className="flex items-center justify-center flex-wrap gap-4 lg:justify-end lg:pr-24 gap-x-8 p-4 md:p-6 lg:p-8
                 shadow-md md:text-3xl 2xl:text-3xl text-center">
-                    <NavLink to="/" end>
-                        Home
-                    </NavLink>
-                    <NavLink to="data" end>
-                        Data
-                    </NavLink>
-                    <NavLink to="watchlist" end>
-                        Watch List
-                    </NavLink>
-                    <NavLink to="login" end>
-                        Log In / Sign Up
-                    </NavLink>
-                    {loggedIn ? (
-                        <button
-                            className="cursor-pointer"
-                            onClick={handleSignOut}
-                        >
-                            Sign Out
-                        </button>
-                    ) : null}
-                </nav>
-            </main>
-        </>
+                <NavLink to="/" end>
+                    Home
+                </NavLink>
+                <NavLink to="data" end>
+                    Data
+                </NavLink>
+                <NavLink to="watchlist" end>
+                    Watch List
+                </NavLink>
+                <NavLink to="login" end>
+                    Log In / Sign Up
+                </NavLink>
+                {loggedIn ? (
+                    <button
+                        className="cursor-pointer"
+                        onClick={handleSignOut}
+                    >
+                        Sign Out
+                    </button>
+                ) : null}
+            </nav>
+        </header>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,6 @@ createRoot(document.getElementById('root')!).render(
         position="bottom-left"
         reverseOrder={false}
       />
-    </BrowserRouter>,
+    </BrowserRouter>
   </AuthContextProvider>
 )
