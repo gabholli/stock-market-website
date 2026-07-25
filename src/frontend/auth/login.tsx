77d95f909c988dbc95ac462046e3421f43aa8ddd
Gabe Hollifield <gholl467@gmail.com>
2026-07-24 20:46:41 -0700

    Added missing await keyword

--- src/frontend/auth/login.tsx
+++ src/frontend/auth/login.tsx
@@ -15,7 +15,7 @@ export default function LogIn() {
             let passwordValue = formData.get("password") as string
             if (!emailValue || !passwordValue) return
 
-            const response = api.post("/auth/login",
+            const response = await api.post("/auth/login",
                 { email: emailValue, password: passwordValue })
 
             if ((await response).data.message) {
